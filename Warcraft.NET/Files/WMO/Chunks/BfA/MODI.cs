1c7c389c17bc51f07a70089f4fd8abb4ccbaf7f3
Luzifix <luzifix19@gmail.com>
2019-05-26 05:46:32 +0200

    Add MODI, GFID, MODD and MODN chunks

---
+++ Warcraft.NET/Files/WMO/Chunks/BfA/MODI.cs
@@ -0,0 +1,79 @@
+﻿using Warcraft.NET.Files.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Warcraft.NET.Files.WMO.Chunks.BfA
+{
+    /// <summary>
+    /// MODI Chunk - Contains a list model file ids.
+    /// </summary>
+    public class MODI : IIFFChunk, IBinarySerializable
+    {
+        /// <summary>
+        /// Holds the binary chunk signature.
+        /// </summary>
+        public const string Signature = "MODI";
+
+        /// <summary>
+        /// Gets or sets the list of file ids in an MODI chunk.
+        /// </summary>
+        public List<uint> ModelFileIds { get; set; } = new List<uint>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MODI"/> class.
+        /// </summary>
+        public MODI()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MODI"/> class.
+        /// </summary>
+        /// <param name="inData">The binary data.</param>
+        public MODI(byte[] inData)
+        {
+            LoadBinaryData(inData);
+        }
+
+        /// <inheritdoc/>
+        public void LoadBinaryData(byte[] inData)
+        {
+            using (var ms = new MemoryStream(inData))
+            using (var br = new BinaryReader(ms))
+            {
+                var offsetCount = inData.Length / sizeof(uint);
+                for (var i = 0; i < offsetCount; ++i)
+                {
+                    ModelFileIds.Add(br.ReadUInt32());
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public string GetSignature()
+        {
+            return Signature;
+        }
+
+        /// <inheritdoc/>
+        public uint GetSize()
+        {
+            return (uint)Serialize().Length;
+        }
+
+        /// <inheritdoc/>
+        public byte[] Serialize(long offset = 0)
+        {
+            using (var ms = new MemoryStream())
+            using (var bw = new BinaryWriter(ms))
+            {
+                foreach (uint modelFileId in ModelFileIds)
+                {
+                    bw.Write(modelFileId);
+                }
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ Warcraft.NET/Files/WMO/Chunks/MODD.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Warcraft.NET.Files.Interfaces;
+using Warcraft.NET.Files.WMO.Entries;
+
+namespace Warcraft.NET.Files.WMO.Chunks
+{
+    public class MODD : IIFFChunk, IBinarySerializable
+    {
+        /// <summary>
+        /// Holds the binary chunk signature.
+        /// </summary>
+        public const string Signature = "MODD";
+
+        /// <summary>
+        /// Gets or sets <see cref="MODDEntry"s />
+        /// </summary>
+        public List<MODDEntry> MODDEntries { get; set; } = new List<MODDEntry>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MODD"/> class.
+        /// </summary>
+        public MODD()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MODD"/> class.
+        /// </summary>
+        /// <param name="inData">The binary data.</param>
+        public MODD(byte[] inData)
+        {
+            LoadBinaryData(inData);
+        }
+
+        /// <inheritdoc/>
+        public string GetSignature()
+        {
+            return Signature;
+        }
+
+        /// <inheritdoc/>
+        public uint GetSize()
+        {
+            return (uint)Serialize().Length;
+        }
+
+        /// <inheritdoc/>
+        public void LoadBinaryData(byte[] inData)
+        {
+            using (var ms = new MemoryStream(inData))
+            using (var br = new BinaryReader(ms))
+            {
+                var materialCount = br.BaseStream.Length / MODDEntry.GetSize();
+                for (var i = 0; i < materialCount; ++i)
+                    MODDEntries.Add(new MODDEntry(br.ReadBytes(MODDEntry.GetSize())));
+            }
+        }
+
+        /// <inheritdoc/>
+        public byte[] Serialize(long offset = 0)
+        {
+            using (var ms = new MemoryStream())
+            using (var bw = new BinaryWriter(ms))
+            {
+                foreach (var material in MODDEntries)
+                    bw.Write(material.Serialize());
+                return ms.ToArray();
+            }
+        }
+    }
+}
